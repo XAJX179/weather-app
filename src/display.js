0ed5c83e89023ec17ad50bbaab98c685b01112e4
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-07-09 16:08:51 +0530

    feat(display): toggle Celsius\Fahrenheit

--- src/display.js
+++ src/display.js
@@ -3,7 +3,9 @@ export const Display = (() => {
   const display = document.querySelector("ul.display");
   const message = document.querySelector("p.message");
   const locationName = document.querySelector("p.location");
-
+  let minTemp;
+  let maxTemp;
+  let temp;
   let minTempItem = document.createElement("li");
   let minTempItemP = document.createElement("p");
   let maxTempItem = document.createElement("li");
@@ -17,16 +19,45 @@ export const Display = (() => {
   display.append(tempItem);
   tempItem.append(tempItemP);
 
+  let toggleScaleBtn = document.createElement("button");
+  toggleScaleBtn.classList.add("toggle-scale");
+  toggleScaleBtn.textContent = "Show in Fahrenheit (F°)";
+  toggleScaleBtn.dataset.scale = "C";
+  toggleScaleBtn.style.display = "none";
+  display.append(toggleScaleBtn);
+
+  toggleScaleBtn.addEventListener("click", toggleScale);
+
+  function toggleScale(e) {
+    if (e.target.dataset.scale == "F") {
+      minTempItemP.textContent = "Minimum : " + minTemp + " C°";
+      maxTempItemP.textContent = "Maximum : " + maxTemp + " C°";
+      tempItemP.textContent = "Average: " + temp + " C°";
+      toggleScaleBtn.dataset.scale = "C";
+      toggleScaleBtn.textContent = "Show in Fahrenheit (F°)";
+    } else {
+      minTempItemP.textContent =
+        "Minimum : " + (minTemp * 1.8 + 32).toFixed(2) + " F°";
+      maxTempItemP.textContent =
+        "Maximum : " + (maxTemp * 1.8 + 32).toFixed(2) + " F°";
+      tempItemP.textContent =
+        "Average: " + (temp * 1.8 + 32).toFixed(2) + " F°";
+      toggleScaleBtn.dataset.scale = "F";
+      toggleScaleBtn.textContent = "Show in Celsius (C°)";
+    }
+  }
+
   function weatherReport(result) {
-    let minTemp = result.days[0].tempmin;
-    let maxTemp = result.days[0].tempmax;
-    let temp = result.days[0].temp;
+    minTemp = result.days[0].tempmin;
+    maxTemp = result.days[0].tempmax;
+    temp = result.days[0].temp;
 
     minTempItemP.textContent = "Minimum : " + minTemp;
     maxTempItemP.textContent = "Maximum : " + maxTemp;
     tempItemP.textContent = "Average: " + temp;
     message.textContent = "";
     locationName.textContent = result.resolvedAddress;
+    toggleScaleBtn.style.display = "initial";
   }
   function getForm() {
     return form;
